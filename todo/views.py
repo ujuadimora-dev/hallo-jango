1bba179ba98cf1f36a87901db6d7f8769c5e2d72
ujuadimora-dev <>
2023-05-26 14:08:36 +0000

    Ediiting an Item

--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from .models import Item
+from .forms  import ItemForm
 
 # Create your views here.
 
 
 def get_todo_list(request):
     items = Item.objects.all()
-    context = {  # dictionary all the item in it, so we net the keys of Items
+    context = {
         'items': items
     }
     return render(request, 'todo/todo_list.html', context)
@@ -14,8 +15,33 @@ def get_todo_list(request):
 
 def add_item(request):
     if request.method == 'POST':
-        name = request.POST.get('item_name')
-        done = 'done' in request.POST
-        Item.objects.create(name=name, done=done)
-        return redirect('get_todo_list')
-    return render(request, 'todo/add_item.html')
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+
+    else:
+        form = ItemForm()
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/add_item.html', context)
+
+
+def edit_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+
+    form = ItemForm(instance=item)
+    context ={
+        'form' : form
+    }
+    return render(request, 'todo/edit_item.html', context)
+
+
+
